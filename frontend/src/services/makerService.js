22ce5318fa0bf24188d9e11c0896b8cebd47dc94
mario pedrao <mariomapedrao@gmail.com>
2026-05-28 09:11:11 -0300

    feat/111 - feat: lancando solicitar cadastro para reflexao na web

---
+++ frontend/src/services/makerService.js
@@ -0,0 +1,13 @@
+import apiClient from '../api/client';
+
+export const makerService = {
+  async solicitarCadastro(dados) {
+    const { data } = await apiClient.post('/maker/cadastro', dados);
+    return data;
+  },
+
+  async listarTipos() {
+    const { data } = await apiClient.get('/tipos');
+    return data;
+  },
+};
